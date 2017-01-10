5cd629581d35aefbc422ab2c23ad07df195ab9ee
Jonathan Delgado Zamorano <Jonathan Delgado Zamorano>
2017-01-10 00:53:00 -0300

    Changed Model: Add method 'isValid'.

--- test/tests.js
+++ test/tests.js
@@ -1,11 +1,41 @@
+const isString = require('lodash/isString')
 const expect = require('expect.js')
+const chance = new (require('chance'))
 const {Model} = require('..')
 
 const log = console.log.bind(console)
 
 describe('VAPI.js Models', () => {
 
-  it('making a instance', () => {
+  describe('Model', () => {
+
+    it('#toString()', () => {
+      const R = chance.first()
+      const E = {[R]: class extends Model {}}
+
+      expect((new (E[R])).toString()).to.be(`[object ${R}]`)
+    })
+
+    it('#isValid()', () => {
+      class MyModelWithValidation extends Model {}
+
+      MyModelWithValidation.defineProperty('name', {
+        validation: isString
+      })
+
+      const instance = new MyModelWithValidation()
+
+      instance.name = 53
+      expect(instance.isValid()).to.not.be.ok()
+
+      instance.name = "hola"
+      expect(instance.isValid()).to.be.ok()
+    })
+
+  })
+
+
+  it('instanceof to a Model', () => {
 
     const instance = new Model()
 
@@ -13,52 +43,52 @@ describe('VAPI.js Models', () => {
 
   })
 
-  describe('describe a new model', () => {
+  // describe('describe a new model', () => {
 
-    it('define a class from a model', () => {
+  //   it('define a class from a model', () => {
 
-      class CustomModel1 extends Model {}
-      class CustomModel2 extends Model {}
+  //     class CustomModel1 extends Model {}
+  //     class CustomModel2 extends Model {}
 
-      CustomModel1.defineProperties({
-        a: {default: 5},
-        b: {_default: 1},
-        c: {_default: 2},
-        d: {_default: 3},
-      })
+  //     CustomModel1.defineProperties({
+  //       a: {default: 5},
+  //       b: {_default: 1},
+  //       c: {_default: 2},
+  //       d: {_default: 3},
+  //     })
 
-      CustomModel2.defineProperties({
-        c: {default: 5},
-        d: {default: 1},
-      })
+  //     CustomModel2.defineProperties({
+  //       c: {default: 5},
+  //       d: {default: 1},
+  //     })
 
-      const instance1 = new CustomModel1()
-      const instance2 = new CustomModel1()
-      const instance3 = new CustomModel2()
+  //     const instance1 = new CustomModel1()
+  //     const instance2 = new CustomModel1()
+  //     const instance3 = new CustomModel2()
 
-      instance2.b = "____5____"
+  //     instance2.b = "____5____"
 
-      instance2.delete('b')
+  //     instance2.delete('b')
 
 
-      log(''+instance1/*.valueOf()*/)
-      log(''+instance2/*.valueOf()*/)
-      log(''+instance3/*.valueOf()*/)
+  //     log(''+instance1/*.valueOf()*/)
+  //     log(''+instance2/*.valueOf()*/)
+  //     log(''+instance3/*.valueOf()*/)
 
-      log()
+  //     log()
 
-      log('instance1')
-      log(JSON.stringify(instance1, null, 2))
-      log()
-      log('instance2')
-      log(JSON.stringify(instance2, null, 2))
-      log()
-      log('instance3')
-      log(JSON.stringify(instance3, null, 2))
+  //     log('instance1')
+  //     log(JSON.stringify(instance1, null, 2))
+  //     log()
+  //     log('instance2')
+  //     log(JSON.stringify(instance2, null, 2))
+  //     log()
+  //     log('instance3')
+  //     log(JSON.stringify(instance3, null, 2))
 
-    })
+  //   })
 
-  })
+  // })
 
 })
 
