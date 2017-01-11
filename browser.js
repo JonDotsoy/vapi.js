6fa6c1549dfac15803f2da7c5c1b5de3c6abf232
Jonathan Delgado Zamorano <Jonathan Delgado Zamorano>
2017-01-11 12:44:18 -0300

    Change Package: Prepare Browser distributión.

--- browser.js
+++ browser.js
@@ -1,2 +1,9 @@
-console.log('load this')
-global.vapi = require('.')
+const Vapi = require('.')
+const name = process.env.npm_package_name || "vapi"
+const nameTitle = process.env.npm_package_title || "VAPI"
+
+global[name] = Vapi
+global[name].version = process.env.npm_package_version
+
+global[nameTitle] = Vapi
+global[nameTitle].version = process.env.npm_package_version
