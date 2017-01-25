b4de79ee037389bd7c694e0960a732b3bf1c95b8
Jonathan Delgado <hi@jon.soy>
2017-01-25 01:30:21 -0300

    Change Package: Added minification with uglify.js.

--- browser.js
+++ browser.js
@@ -1,6 +1,9 @@
+require('babel-polyfill')
 const Vapi = require('.')
 const name = process.env.npm_package_name || 'vapi'
 const nameTitle = process.env.npm_package_title || 'VAPI'
+
+console.warn(`The vapi to browser use 'babel-polyfill'. To load this module is recommendable "require" or "import" to load the vapi module on you aplicatión.`)
 
 global[name] = Vapi
 global[name].version = process.env.npm_package_version
